26bf132d4369bc36da482fac0d1877266a908b7a
Nikhil <97027499+Nikhilgautam12345@users.noreply.github.com>
2025-04-21 17:30:36 +0530

    update husky

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-      optimizeCss: false, // ← disables Lightning CSS
-    },
-  };
-  
-  export default nextConfig; // use `module.exports = nextConfig;` if not using ESM
-  
+const nextConfig = {};
+
+export default nextConfig;
